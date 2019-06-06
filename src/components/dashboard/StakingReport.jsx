a9ff27aaffd9bc52b351690741defd3c1798423b
dantudor <dtudor01@gmail.com>
2019-06-06 10:05:53 +0100

    Remove nbsp, add padding

--- src/components/dashboard/StakingReport.jsx
+++ src/components/dashboard/StakingReport.jsx
@@ -11,10 +11,17 @@ import {reportActions as actions} from "../../actions";
 const styles = () => ({
   tableCell: {
     padding: "4px 0 4px 0",
+    width: '43%',
   },
   tableCenter: {
     textAlign: "center",
     padding: "4px 0 4px 0",
+    width: '13%',
+  },
+  tableRight: {
+    textAlign: "right",
+    padding: "4px 0 4px 0",
+    width: '43%',
   },
 })
 
@@ -24,7 +31,7 @@ class StakingReport extends React.Component {
   }
 
   render() {
-    const {classes, report} = props
+    const {classes, report} = this.props
     const loaded = !report.loadingReportStaking && !report.errorReportStaking
 
     return (
@@ -33,35 +40,35 @@ class StakingReport extends React.Component {
           <TableHead>
             <TableRow>
               <TableCell className={classes.tableCell} variant="head">Period</TableCell>
-              <TableCell className={classes.tableCenter} variant="head">#&nbsp;Stakes</TableCell>
-              <TableCell className={classes.tableCenter} variant="head">Stake Reward</TableCell>
+              <TableCell className={classes.tableCenter} variant="head">Stakes</TableCell>
+              <TableCell className={classes.tableRight} variant="head">Stake Reward</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             <TableRow>
               <TableCell className={classes.tableCell}>Last&nbsp;24&nbsp;hours</TableCell>
               <TableCell className={classes.tableCenter}>{ loaded ? report.staking.last24Hours.stakes : '--'}</TableCell>
-              <TableCell className={classes.tableCenter}>{ loaded ? navFormat(report.staking.last24Hours.balance)+"&nbsp;Nav" : '--' }</TableCell>
+              <TableCell className={classes.tableRight}>{ loaded ? navFormat(report.staking.last24Hours.balance)+" Nav" : '--' }</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className={classes.tableCell}>Last&nbsp;7&nbsp;days</TableCell>
               <TableCell className={classes.tableCenter}>{ loaded ? report.staking.last7Days.stakes : '--'}</TableCell>
-              <TableCell className={classes.tableCenter}>{ loaded ? navFormat(report.staking.last7Days.balance)+"&nbsp;Nav" : '--' }</TableCell>
+              <TableCell className={classes.tableRight}>{ loaded ? navFormat(report.staking.last7Days.balance)+" Nav" : '--' }</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className={classes.tableCell}>Last&nbsp;30&nbsp;days</TableCell>
               <TableCell className={classes.tableCenter}>{ loaded ? report.staking.last30Days.stakes : '--'}</TableCell>
-              <TableCell className={classes.tableCenter}>{ loaded ? navFormat(report.staking.last30Days.balance)+"&nbsp;Nav" : '--' }</TableCell>
+              <TableCell className={classes.tableRight}>{ loaded ? navFormat(report.staking.last30Days.balance)+" Nav" : '--' }</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className={classes.tableCell}>Last&nbsp;year</TableCell>
               <TableCell className={classes.tableCenter}>{ loaded ? report.staking.lastYear.stakes : '--'}</TableCell>
-              <TableCell className={classes.tableCenter}>{ loaded ? navFormat(report.staking.lastYear.balance)+"&nbsp;Nav" : '--' }</TableCell>
+              <TableCell className={classes.tableRight}>{ loaded ? navFormat(report.staking.lastYear.balance)+" Nav" : '--' }</TableCell>
             </TableRow>
             <TableRow>
               <TableCell className={classes.tableCell}>All time</TableCell>
               <TableCell className={classes.tableCenter}>{ loaded ? report.staking.all.stakes : '--'}</TableCell>
-              <TableCell className={classes.tableCenter}>{ loaded ? navFormat(report.staking.all.balance)+"&nbsp;Nav" : '--' }</TableCell>
+              <TableCell className={classes.tableRight}>{ loaded ? navFormat(report.staking.all.balance)+" Nav" : '--' }</TableCell>
             </TableRow>
           </TableBody>
         </Table>
